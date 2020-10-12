1f8bca7b1353475b6b3ce8b39c9b76699d5d16cf
Muhammad Osaid <muhammadosaid06@gmail.com>
2020-10-13 04:33:39 +0800

    Fix bugs due to homepage setting error

---
+++ precache-manifest.88e818199b4717ca6d0dca4a07838466.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a2a33285116f7d9c0334b6108eff408",
+    "url": "./index.html"
+  },
+  {
+    "revision": "84ab636ad8b5aea2eab7",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f716d94ddc19376c2496",
+    "url": "./static/js/2.51231538.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.51231538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84ab636ad8b5aea2eab7",
+    "url": "./static/js/main.f8362ca5.chunk.js"
+  },
+  {
+    "revision": "3fad2e394ec9721c917c",
+    "url": "./static/js/runtime-main.1d4114e7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1930afdfdc9782486dfc68376fa56ed2.js"
+  "./precache-manifest.88e818199b4717ca6d0dca4a07838466.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
